2b507af269327e0297b830df0aa3af8b950b7710
Omkar Kabde <omkarkabde@gmail.com>
2024-01-30 14:27:27 +0530

    Added file upload functionality

--- app.py
+++ app.py
@@ -1,16 +1,42 @@
 import pymongo
-from flask import Flask
+from flask import Flask, request, jsonify
+import pandas as pd
+from pathlib import Path
+import numpy as np
+import json
 
 cred=open("connection.txt").read()
-client=pymongo.MongoClient(cred)
-db=client['text_db5']
-col=db['column_1']
 
 app=Flask(__name__)
 
 @app.route('/upload/<path:filepath>',methods=['GET'])
 def upload(filepath):
-    pass
+    if Path(filepath).suffix in ['.xls','.xlsx']:
+        df=pd.read_excel(filepath)
+    elif Path(filepath).suffix == '.csv':
+        df=pd.read_csv(filepath)
+    else:
+        return jsonify({"Failure":"Invalid File Format"})
+
+    client=pymongo.MongoClient(cred)
+    db=client['text_db5']
+    col=db['column_1']
+
+    curr_id=col.find().sort('_id', -1).limit(1)[0]['_id']+1
+
+    df['_id']=list(range(curr_id,len(df)+curr_id))
+    if 'tags' not in df:
+        df['tags'] = np.empty((len(df), 0)).tolist()
+    df=df[['_id','text','tags']]
+
+    data=json.loads(df.to_json(orient="records"))
+    try:
+        x = col.insert_many(data)
+    except:
+        return jsonify({"Failure":"File upload failed"})
+    # return json.dumps(json.loads(data),indent=2)
+    return jsonify({"Success":"File uploaded successfully"})
+
 
 @app.route('/download/<filetype>',methods=['GET'])
 def download(filetype):
